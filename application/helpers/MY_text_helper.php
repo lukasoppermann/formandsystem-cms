7be3c9d710398abf3801bfedba010558dc1c0f07
Lukas Oppermann <oppermann.lukas@googlemail.com>
2012-04-16 14:57:39 +1000

    cleanup of text_limiter & closetags in my_text_helper & removing my_string_helper

--- application/helpers/MY_text_helper.php
+++ application/helpers/MY_text_helper.php
@@ -48,7 +48,7 @@ function text_limiter($text = null, $count = 150, $strip_tags = false, $end = '&
 	// if tags are not striped, close open tags
 	if( $strip_tags != TRUE )
 	{
-		closetags($text);
+		$text = closetags($text);
 	}
 	// return $text
 	return $text;
@@ -62,32 +62,37 @@ function text_limiter($text = null, $count = 150, $strip_tags = false, $end = '&
  */
 function closetags($html) 
 {
-	// put all opened tags into an array
+	// collect open tags into an array
 	preg_match_all('#<([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $html, $result);
-	$openedtags = $result[1];
-
-	// put all closed tags into an array
+	$opened_tags = $result[1];
+	// collect close tags into an array
 	preg_match_all('#</([a-z]+)>#iU', $html, $result);
-	$closedtags = $result[1];
-	$len_opened = count($openedtags);
-	// all tags are closed
-	if (count($closedtags) == $len_opened) 
+	$closed_tags = $result[1];
+	// count open tags
+	$count_open = count($opened_tags);
+	// if all tags are closed
+	if( count($closed_tags) == $count_open ) 
 	{
 		return $html;
 	}
-	$openedtags = array_reverse($openedtags);
-	// close tags
-	for ($i=0; $i < $len_opened; $i++) 
+	// if open tags remain
+	$opened_tags = array_reverse($opened_tags);
+	// loop through tags
+	for( $i=0; $i < $count_open; $i++ ) 
 	{
-		if (!in_array($openedtags[$i], $closedtags))
+		// if tag is not closed
+		if( !in_array($opened_tags[$i], $closed_tags) )
 		{
-			$html .= '</'.$openedtags[$i].'>';
+			// close
+			$html .= '</'.$opened_tags[$i].'>';
 		} 
 		else 
 		{
-			unset($closedtags[array_search($openedtags[$i], $closedtags)]);
+			// unset from array
+			unset($closed_tags[array_search($opened_tags[$i], $closed_tags)]);
 		}
 	}
+	// return html
 	return $html;
 }
 /* End of file MY_text_helper.php */
