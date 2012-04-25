d1a690afcef945bc4c442ef821fbb65c684df8f8
Lukas Oppermann <oppermann.lukas@googlemail.com>
2012-04-25 20:31:51 +1000

    add email config

---
+++ application/config/email.php
@@ -0,0 +1,14 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+| -------------------------------------------------------------------------
+| Email
+| -------------------------------------------------------------------------
+| This file lets you configure the email class.
+| Please see the user guide for info:
+|
+|	http://codeigniter.com/user_guide/libraries/email.html
+|
+*/
+$config['mailtype'] = 'html';
+/* End of file email.php */
+/* Location: ./application/config/email.php */
