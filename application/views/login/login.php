a1abb2a14abb3034b74b89efa3b5b77a5db93594
Lukas Oppermann <oppermann.lukas@googlemail.com>
2012-05-19 19:03:02 +1000

    moving login templates to login folder

---
+++ application/views/login/login.php
@@ -0,0 +1,6 @@
+<?
+	// fs_log('Send emails to retrieve password / user');
+?>
+<form action="<?=$url?>" method="post" accept-charset="utf-8" name="login" id="login">
+	<?=variable($users);?>
+</form>
