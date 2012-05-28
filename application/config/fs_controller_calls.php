73fd2156769730874133220153d862d9e8f06a20
Lukas Oppermann <oppermann.lukas@googlemail.com>
2012-05-29 08:26:14 +1000

    adding call config file

---
+++ application/config/fs_controller_calls.php
@@ -0,0 +1,41 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/*
+|--------------------------------------------------------------------------
+| Define Methods
+|--------------------------------------------------------------------------
+*/
+$config['methods'] 	= array(
+	'dashboard' 	=> array(
+	),
+	'content' 		=> array(
+		'default' 		=> 'general',
+		'general' 		=> 'general',
+	),
+	'media' 		=> array(
+		'default' 		=> 'show'
+	),
+	'user' 			=> array(
+		'default' 		=> 'general',
+		'general' 		=> 'general',
+		'languages' 	=> 'languages',
+		'seo' 			=> 'seo',
+		'search' 		=> 'search',
+		'cms' 			=> 'cms'
+	),
+	'settings' 		=> array(
+		'default' 		=> 'general',
+		'general' 		=> 'general',
+		'languages' 	=> 'languages',
+		'seo' 			=> 'seo',
+		'search' 		=> 'search',
+		'cms' 			=> 'cms'
+	),
+	'profile' 		=> array(
+		'default' 		=> 'general',
+		'general' 		=> 'general',
+		'languages' 	=> 'languages',
+		'seo' 			=> 'seo',
+		'search' 		=> 'search',
+		'cms' 			=> 'cms'
+	)
+);
