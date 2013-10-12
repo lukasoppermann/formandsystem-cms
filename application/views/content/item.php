ea594a94dc4d7d101e7f7c38b6bbb77ecea472a2
Lukas Oppermann <oppermann.lukas@googlemail.com>
2013-10-12 09:06:27 +0200

    removing stuff

--- application/views/content/item.php
+++
@@ -1,3 +0,0 @@
-<li>
-	<a href="<?=active_url()?>content/edit/<?=$id?>"><?=$title?></a> (<?=($status == 1 ? 'published' : 'draft')?>)
-</li>
--- application/views/login/login.php
+++
@@ -1,3 +0,0 @@
-<form action="<?=$url?>" method="post" accept-charset="utf-8" name="login" id="login" class="form<?=variable($load_user)?>">
-	<?=variable($users);?>
-</form>
