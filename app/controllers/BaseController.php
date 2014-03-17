cf05447caf65e1a4faf082038da1360e519dfcf8
Lukas Oppermann <lukas@vea.re>
2014-03-17 09:10:14 +0100

    working

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -3,12 +3,12 @@
 * Utility FN
 */
 function variable( &$var = null )
-{ 
+{
 	if( isset($var) )
 	{
 		return $var;
 	}
-	return false; 
+	return false;
 }
 
 class BaseController extends Controller {
@@ -23,15 +23,16 @@ class BaseController extends Controller {
 		// set charset
 		Header("Content-type: text/html;charset=UTF-8");
 		// set header for browser to not cache stuff
-		Header("Last-Modified: ". gmdate( "D, j M Y H:i:s" ) ." GMT"); 
-		Header("Expires: ". gmdate( "D, j M Y H:i:s", time() ). " GMT"); 
-		Header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1 
-		Header("Cache-Control: post-check=0, pre-check=0", FALSE); 
+		Header("Last-Modified: ". gmdate( "D, j M Y H:i:s" ) ." GMT");
+		Header("Expires: ". gmdate( "D, j M Y H:i:s", time() ). " GMT");
+		Header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+		Header("Cache-Control: post-check=0, pre-check=0", FALSE);
 		Header("Pragma: no-cache" ); // HTTP/1.0
 		// add css resources
 		Optimization::css(array('reset', 'layout', 'nav', 'icons', 'contentediting'));
 		// add js resources
-		// Optimization::js('minified, fs.media_queries, offset, viewport, onScroll, scrollPercent, scrollOffset, base');
+		// TODO: Fix Optimization js
+		Optimization::js(array('mark', 'content'));
 		// google analytics
 		// Optimization::add_lines('js',"var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-7074034-1']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();");
 	}
@@ -47,28 +48,28 @@ class BaseController extends Controller {
 			$this->layout = View::make($this->layout)->with('nav', $this->navigation() );
 		}
 	}
-	
+
 	protected function navigation()
-	{		
+	{
 		$cont = new Content;
 		$content = $cont->getArray();
 		$contentByMenu = $cont->byMenu();
-		
+
 		$navi = new Navigation;
 		$nav = $navi->getArray( $cont->byMenu() );
-		
+
 		// echo("<pre>");print_r($nav);echo("</pre>");
-		
+
 		$language = 'en';
-		
+
 		// foreach($nav as $key => $item)
-		// {	
+		// {
 		// 	if( isset($contentByMenu[$key]) && isset($contentByMenu[$key]['language'][$language]) )
 		// 	{
 		// 		$nav[$key]['content'] = $contentByMenu[$key]['language'][$language];
 		// 	}
 		// }
-		// 
+		//
 		return $nav;
 	}
 
---
+++ public/libs/js/content.js
@@ -0,0 +1,52 @@
+'use strict'
+
+// run codemirror on every instance of .mark
+Array.prototype.slice.call(document.getElementsByClassName('mark'),0).forEach(function(editor){
+
+	options.cm = CodeMirror.fromTextArea(editor, {
+		theme: "mark",
+    // value: "function myScript(){return 100;}\n",
+		mode: {
+			name: "gfm",
+			highlightFormatting: true
+		},
+		lineNumbers: true,
+		addModeClass: false,
+		lineWrapping: true,
+		flattenSpans: true,
+		cursorHeight: 1,
+		matchBrackets: true,
+		autoCloseBrackets: { pairs: "()[]{}''\"\"", explode: "{}" },
+		matchTags: true,
+		showTrailingSpace: true,
+		autoCloseTags: true,
+		styleSelectedText: false,
+		styleActiveLine: true,
+		placeholder: "",
+      tabMode: 'indent',
+		tabindex: "2",
+		dragDrop: false,
+		extraKeys: {
+			"Enter": "newlineAndIndentContinueMarkdownList",
+			"Cmd-B": function(){
+				options.fn.inlineFormat({'format':'strong'});
+			},
+			"Ctrl-B": function(){
+				options.fn.inlineFormat({'format':'strong'});
+			},
+			"Cmd-I": function(){
+				options.fn.inlineFormat({'format':'em'});
+			},
+			"Ctrl-I": function(){
+				options.fn.toggleFormat('quote', {"level":2});
+			}
+		}
+	});
+	// add edit Options
+	options.cm.on("cursorActivity", function(){
+		editOptions();
+		// options.fn.inlineFormat({'format':'em'});
+						// console.log( '##'+options.cm.getSelection().match( /(?:^|[^_*])_*([*](?:[*]{2})*)?[^*_]+\1_*(?:[^*_]|$)/gm )+'##' );
+	});
+
+});
