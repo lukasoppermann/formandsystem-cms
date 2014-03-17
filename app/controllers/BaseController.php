8a5cc234443fad395d001079415d2f7ac943328b
Lukas Oppermann <lukas@vea.re>
2014-03-17 09:24:52 +0100

    update

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -32,7 +32,7 @@ class BaseController extends Controller {
 		Optimization::css(array('reset', 'layout', 'nav', 'icons', 'contentediting'));
 		// add js resources
 		// TODO: Fix Optimization js
-		Optimization::js(array('mark', 'content'));
+		Optimization::js(array('codemirror','mark', 'content'));
 		// google analytics
 		// Optimization::add_lines('js',"var _gaq = _gaq || [];_gaq.push(['_setAccount', 'UA-7074034-1']);_gaq.push(['_trackPageview']);(function() {var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);})();");
 	}
