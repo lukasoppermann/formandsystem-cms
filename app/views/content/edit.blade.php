93f87fb661adcaf918bc001d5ca8680ea2c7f95e
Lukas Oppermann <lukas@vea.re>
2014-03-19 09:16:23 +0100

    working on mark integration

--- app/views/content/edit.blade.php
+++ app/views/content/edit.blade.php
@@ -6,13 +6,10 @@
 		<div class="button blue save">Save</div>
 	</div>
 	<input class="headline" type="text" placeholder="Type your title" value="{{$content['title']}}" />
-	
 	@foreach ( $content['content'] as $section )
-	
+
 		@foreach ( $section['content'] as $block )
-		<?
-		// echo("<pre>");print_r($block);echo("</pre>");
-		?> 
+
 			<div class="block span {{ variable($block['class']) }}">
 			@if ($block['type'] === 'text')
 				<textarea class="mark">{{ variable($block['content']) }}</textarea>
@@ -21,8 +18,5 @@
 			</div>
 		@endforeach
 	@endforeach
-	
-	<?
-	// echo("<pre>");print_r($content);echo("</pre>");
-	?>
+
 </div>
