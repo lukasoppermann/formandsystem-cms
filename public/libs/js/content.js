389fd72421a340660f8e9db45f6acdedbb965369
Lukas Oppermann <lukas@vea.re>
2014-03-28 11:53:29 +0100

    fix

--- public/libs/js/content.js
+++ public/libs/js/content.js
@@ -1,9 +1,7 @@
 'use strict';
 
 // run codemirror on every instance of .mark
-Array.prototype.slice.call(document.getElementsByClassName('mark'),0).forEach(function(editor){
-	mark(editor, {
-		excludePanel: ['code'],
-		lineNumbers: false
-	});
+mark(document.getElementsByClassName('mark'), {
+	excludePanel: ['code'],
+	lineNumbers: false
 });
