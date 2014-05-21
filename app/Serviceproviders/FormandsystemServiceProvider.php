c2bf971e03d0d44681699f187d525a6b391090f8
Lukas Oppermann <lukas@vea.re>
2014-05-21 16:48:19 +0200

    adding facades and serviceproviders

---
+++ app/Serviceproviders/FormandsystemServiceProvider.php
@@ -0,0 +1,35 @@
+<?php namespace App\Serviceproviders;
+
+use Illuminate\Support\ServiceProvider;
+
+class FormandsystemServiceProvider extends ServiceProvider {
+	
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+	
+	/**
+	 * Bind classes & register facades
+	 */
+	public function register()
+  {
+		/**
+		 * Bind classes to app
+		 */
+    $this->app->bind('content', 'ContentModel');
+    $this->app->bind('navigation', 'NavigationModel');
+		/**
+		 * Register Facades
+		 */
+    $this->app->booting(function()
+    {
+      $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+			$loader->alias('Content', '\App\Facades\Content');
+			$loader->alias('Navigation', '\App\Facades\Navigation');
+    });
+  }
+
+}
