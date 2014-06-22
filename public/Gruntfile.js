19d687e1c95e4dd5120c9e4cdb67f4bc8ffaa288
Lukas Oppermann <lukas@vea.re>
2014-06-22 16:57:22 +0200

    adding grid

--- public/Gruntfile.js
+++ public/Gruntfile.js
@@ -3,31 +3,15 @@ module.exports = function(grunt) {
 	var pkg = grunt.file.readJSON('package.json');
   // Project configuration.
   grunt.initConfig({
-		globalConfig = {
+		config: {
 			layout: 'layout',
 			images: 'images',
 			css: 'css',
 			js: 'js',
 			bower_path: 'bower_components'
 		},
-    meta: {
-			version: '0.3.0',
-			project: 'Test',
-			website: 'http://vea.re',
-			dev_name: 'Lukas',
-    },
-    // Metadata.
-    banner: '/*! <%= meta.project %> - v<%= meta.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '* <%= meta.website %>\n' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
-      '<%= meta.dev_name %>; Licensed MIT */\n',
     // Task configuration.
     concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
       dist: {
         src: ['libs/js/*.js'],
         dest: 'libs/js/application.min.js'
@@ -63,7 +47,6 @@ module.exports = function(grunt) {
         }]
       }
 		},
-		
     autoprefixer: {
       options: {
         browsers: ['last 4 version', 'ie 8', 'ie 9']
@@ -71,53 +54,10 @@ module.exports = function(grunt) {
 	    multiple_files: {
 	      expand: true,
 	      flatten: true,
-	      src: 'libs/css/*.css' // -> src/css/file1.css, src/css/file2.css
-	      // dest: 'dest/css/' // -> dest/css/file1.css, dest/css/file2.css
-	    },
+	      src: '<%= config.css %>/*.css', // -> src/css/file1.css, src/css/file2.css
+	      dest: '<%= config.css %>/' // -> dest/css/file1.css, dest/css/file2.css
+	    }
 	  },
-		csslint: {
-      lax: {
-        options: {
-					'import': 2,
-					'box-sizing': false,
-					'outline-none': false,
-					'ids': false,
-					'adjoining-classes': false,
-					'box-model': false,
-					'fallback-colors': false,
-					'compatible-vendor-prefixes': false
-        },
-        src: ['libs/css/*.css']
-			},
-		},
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        unused: true,
-        boss: true,
-        eqnull: true,
-        browser: true,
-        globals: {
-          jQuery: true
-        }
-      },
-      // gruntfile: {
-      //   src: 'Gruntfile.js'
-      // },
-      lib_test: {
-        src: ['libs/**/*.js', 'test/**/*.js']
-      }
-    },
-    qunit: {
-      files: ['test/**/*_test.js']
-    },
 		svgstore: {
 	    options: {
 	      prefix : 'icon-', // This will prefix each ID
@@ -135,21 +75,24 @@ module.exports = function(grunt) {
 	      },
 	    },
 	  },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test']
-      },
-		  svg_sprite: {
-		    files: ['layout/**/*.svg'],
-		    tasks: ['svgstore'],
+	  jshint: {
+	    all: ['Gruntfile.js']
+	  },
+		watch: {
+			jshint: {
+				files: ['Gruntfile.js'],
+				tasks: ['jshint']
+			},
+			svgsprite: {
+				files: ['layout/**/*.svg'],
+				tasks: ['svgstore']
+			},
+			css: {
+				files: ['<%= config.css %>/*.css','!<%= config.css %>/*.min.css'],
+				tasks: ['autoprefixer']
 			}
-    }
-  });
+		}
+	});
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-concat');
@@ -157,17 +100,16 @@ module.exports = function(grunt) {
 	// https://www.npmjs.org/package/grunt-contrib-imagemin
 	grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-	
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-csslint');
+	grunt.loadNpmTasks('grunt-replace');
+	grunt.loadNpmTasks('grunt-shell');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 	//https://www.npmjs.org/package/grunt-autoprefixer
 	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-svg-sprite');
 	grunt.loadNpmTasks('grunt-svgstore');
   // Default task.
-	grunt.registerTask('default', ['jshint', 'csslint', 'autoprefixer']);
+	grunt.registerTask('default', ['autoprefixer','svgstore']);
   // Build task.
-	grunt.registerTask('build', ['jshint', 'csslint', 'autoprefixer', 'concat', 'uglify', 'cssmin']);
+	grunt.registerTask('build', ['autoprefixer', 'concat', 'uglify', 'cssmin']);
 
 };
