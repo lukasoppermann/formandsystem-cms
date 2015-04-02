d24dafddbcdc79e4e75bebc3ce1208e655813ecd
Lukas Oppermann <lukas@vea.re>
2015-04-02 18:43:23 +0200

    working on better css structure

--- resources/views/partials/menu-item.blade.php
+++ resources/views/partials/menu-item.blade.php
@@ -9,27 +9,27 @@ $content = $item['content'][\Config::get('content.locale')];
   <div class="menu-item{{ \Request::is('pages/'.$content['link']) ? ' is-active js-is-active' : ''}}">
 
     <a class="menu-link o-media" rel="dns-prefetch" data-id="1" href="{{url("pages/".$content['link'])}}">
-      <svg viewBox="0 0 512 512" class="icon-page o-media__figure">
+      <svg viewBox="0 0 512 512" class="o-icon--white o-media__figure">
         <use xlink:href="#icon-page"></use>
       </svg>
       <span class="menu-link-text menu-link-fade-right menu-link-fade-right o-media__body">{{$content['menu_label']}}</span>
     </a>
 
     <a href="#visible" class="menu-item-status{{ ($content['published'] === 1 ? ' is-published': '')}} menu-link-icon menu-link--icon-right">
-      <svg viewBox="0 0 512 512" class="icon-eye icon--right">
+      <svg viewBox="0 0 512 512" class="icon-eye o-icon--white">
         <use xlink:href="#icon-eye"></use>
       </svg>
-      <svg viewBox="0 0 512 512" class="icon-eye-closed icon--right">
+      <svg viewBox="0 0 512 512" class="icon-eye-closed o-icon--white">
         <use xlink:href="#icon-eye-closed"></use>
       </svg>
     </a>
   </div>
   <div class="menu-item-options{{ \Request::is('pages/'.$content['link']) ? ' is-active':''}}">
-    <label class="menu-item-options-link">
-      <svg viewBox="0 0 512 512" class="icon-link icon--left">
+    <label class="menu-item-options-link o-media o-media--padded-md-sm">
+      <svg viewBox="0 0 512 512" class="o-icon--white o-media__figure">
         <use xlink:href="#icon-link"></use>
       </svg>
-      <input class="link-input" type="text" value="{{$content['link'] or ''}}" placeholder="/link-to-page" />
+      <input class="link-input o-media__body" type="text" value="{{$content['link'] or ''}}" placeholder="/link-to-page" />
     </label>
     <div class="menu-item-options-bottom">
       <a class="menu-item-options-delete" href="'.url('/content/destroy/'.$itemContent['id']).'">delete</a>
