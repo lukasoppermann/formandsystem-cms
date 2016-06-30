72e10e88083d0702a2f5e09abe87f354248681b9
Lukas Oppermann <lukas@vea.re>
2016-06-30 13:48:40 +0200

    creating all new Entities

--- app/Entities/Fragment.php
+++ app/Entities/Fragment.php
@@ -2,10 +2,34 @@
 
 namespace App\Entities;
 
+use App\Entities\AbstractCollectionEntity;
 use Illuminate\Support\Collection as LaravelCollection;
+use Cache;
+use App\Services\Api\FragmentService;
 
-class Fragment extends AbstractResourceEntity
+class Fragment extends AbstractApiResourceEntity
 {
+    /**
+     * get data for this entity
+     *
+     * @method getData
+     *
+     * @param  string   $id
+     *
+     * @return Illuminate\Support\Collection
+     */
+    protected function getData($id){
+        if(!Cache::has($id)){
+            // throw expection if account is not found
+            if( !$item = (new ImageService)->first('id', $id) ){
+                throw new \EmptyException('No '.get_class($this).' with ID: '.$id.' found.');
+            }
+            // store item in cache
+            Cache::put($id,$item,1440);
+        }
+        // return from cache
+        return new LaravelCollection(Cache::get($id));
+    }
     /**
      * transform attributes
      *
@@ -15,48 +39,43 @@ class Fragment extends AbstractResourceEntity
      *
      * @return array
      */
-    protected function attributes(Array $attributes, $rel = NULL)
+    protected function attributes($attributes)
     {
-        $details = new LaravelCollection;
-        //
-        // if(isset($rel['relationships']) && count($rel['relationships']) > 0){
-        //     if(isset($rel['relationships']['metadetails']) && count($rel['relationships']['metadetails']) > 0)
-        //     $rel['relationships']->get('metadetails')->each(function($item) use ($details){
-        //         if($details->has($item->type)){
-        //             $value = $details->get($item->type);
-        //             $value = is_array($value) ? $value : [$value];
-        //             $details->put($item->type, array_merge([$item->data],$value));
-        //         }else{
-        //             $details->put($item->type, $item->data);
-        //         }
-        //     });
-        // }
-        $related = [
-            'details' => new LaravelCollection,
-            'images' => new LaravelCollection,
-        ];
-
-        if(isset($rel['relationships']) && count($rel['relationships']) > 0){
-
-            if(isset($rel['relationships']['metadetails']) && count($rel['relationships']['metadetails']) > 0)
-            $rel['relationships']->get('metadetails')->each(function($item) use ($details){
-                if($details->has($item->type)){
-                    $value = $details->get($item->type);
-                    $value = is_array($value) ? $value : [$value];
-                    $details->put($item->type, array_merge([$item->data],$value));
-                }else{
-                    $details->put($item->type, $item->data);
-                }
-            });
-        }
         // return attributes
-        return array_merge([
-            'type'         => $attributes['type'],
-            'name'         => $attributes['name'],
-            'data'         => $this->json_decode($attributes['data']),
-            'created_at'   => $attributes['created_at'],
-            'is_trashed'   => $attributes['is_trashed'],
-            'details'      => $details,
-        ], $this->related());
+        return [
+            'id'            => $attributes['id'],
+            'resource_type' => $attributes['type'],
+            'type'         => $attributes['attributes']['type'],
+            'name'         => $attributes['attributes']['name'],
+            'data'         => $this->json_decode($attributes['attributes']['data']),
+            'created_at'   => $attributes['attributes']['created_at'],
+            'is_trashed'   => $attributes['attributes']['is_trashed'],
+        ];
+    }
+    /**
+     * validate user data
+     *
+     * @method validateUpdate
+     *
+     * @param  array          $data [description]
+     *
+     * @return array
+     */
+    protected function validateUpdate(array $data)
+    {
+        return $data;
+    }
+    /**
+     * validate user data
+     *
+     * @method validateCreate
+     *
+     * @param  array          $data [description]
+     *
+     * @return array
+     */
+    protected function validateCreate(array $data)
+    {
+        return $data;
     }
 }
--- app/Entities/Image.php
+++ app/Entities/Image.php
@@ -2,8 +2,34 @@
 
 namespace App\Entities;
 
-class Image extends AbstractResourceEntity
+use App\Entities\AbstractCollectionEntity;
+use Illuminate\Support\Collection as LaravelCollection;
+use Cache;
+use App\Services\Api\ImageService;
+
+class Image extends AbstractApiResourceEntity
 {
+    /**
+     * get data for this entity
+     *
+     * @method getData
+     *
+     * @param  string   $id
+     *
+     * @return Illuminate\Support\Collection
+     */
+    protected function getData($id){
+        if(!Cache::has($id)){
+            // throw expection if account is not found
+            if( !$item = (new ImageService)->first('id', $id) ){
+                throw new \EmptyException('No '.get_class($this).' with ID: '.$id.' found.');
+            }
+            // store item in cache
+            Cache::put($id,$item,1440);
+        }
+        // return from cache
+        return new LaravelCollection(Cache::get($id));
+    }
     /**
      * transform attributes
      *
@@ -13,12 +39,40 @@ class Image extends AbstractResourceEntity
      *
      * @return array
      */
-    protected function attributes(Array $attributes, $rel = NULL)
+    protected function attributes($attributes)
     {
         return [
-            'filename'      => $attributes['filename'],
-            'slug'          => $attributes['slug'],
-            'link'          => trim(config('site_url'),'/').'/'.trim(config('img_dir'),'/').'/'.$attributes['filename'],
+            'id'            => $attributes['id'],
+            'resource_type' => $attributes['type'],
+            'filename'      => $attributes['attributes']['filename'],
+            'slug'          => $attributes['attributes']['slug'],
+            'link'          => trim(config('site_url'),'/').'/'.trim(config('img_dir'),'/').'/'.$attributes['attributes']['filename'],
         ];
+    }
+    /**
+     * validate user data
+     *
+     * @method validateUpdate
+     *
+     * @param  array          $data [description]
+     *
+     * @return array
+     */
+    protected function validateUpdate(array $data)
+    {
+        return $data;
+    }
+    /**
+     * validate user data
+     *
+     * @method validateCreate
+     *
+     * @param  array          $data [description]
+     *
+     * @return array
+     */
+    protected function validateCreate(array $data)
+    {
+        return $data;
     }
 }
