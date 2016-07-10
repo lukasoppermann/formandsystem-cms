c74c462a84b8dc4d60a42f3697e518484a462b6c
Lukas Oppermann <lukas@vea.re>
2016-07-10 11:24:24 +0200

    fragments sortable

--- resources/views/fragments/fragment.blade.php
+++ resources/views/fragments/fragment.blade.php
@@ -4,16 +4,20 @@ if(!in_array($item->get('type'), ['image','section','text','collection','input']
 }
 ?>
 <div class="{{$sortable_class or ''}} o-fragment o-fragment--{{$item->get('type')}} o-grid__column o-grid__user-column--md-{{$item->metadetails('type','columns_medium',true)->get('data', 12)}}of{{config('user.grid-md')}} {{$custom_class or ''}}" data-id="{{$item->get('id')}}">
+
+    {{-- Handle for sortable --}}
     @if($sortable_class !== NULL)
         <div class="{{$sortable_class}}-handle c-sortable-fragment__handle"></div>
     @endif
-    {{$item->get('position')}}
+
+    {{-- Settings Icon --}}
     <div class="c-settings-panel__toggle c-settings-panel__toggle--right c-settings-panel__toggle--small" data-toggle-dialog="fragment-settings-{{$item->get('id')}}" data-dialog-link="/dialog/fragmentSettings?id={{$item->get('id')}}">
         <svg viewBox="0 0 512 512" class="o-icon">
           <use xlink:href="#svg-icon--settings"></use>
         </svg>
     </div>
 
+    {{-- Fragment content --}}
     @if (in_array($item->get('type'), ['image','section','text','collection','input']))
         @include('fragments.'.$item->get('type'), [
             'fragment' => $item
@@ -21,7 +25,5 @@ if(!in_array($item->get('type'), ['image','section','text','collection','input']
     @else
         @include('fragments.custom', ['fragment' => $item])
     @endif
-
-
 
 </div>
