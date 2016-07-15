7c1a3c04c1bad429f23cbf53b635966533710013
Lukas Oppermann <lukas@vea.re>
2016-07-15 17:27:26 +0200

    adding capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+# config valid only for Capistrano 3.1
+# require 'capistrano/ext/multistage'
+lock '3.5.0'
+
+set :stages, ["production"]
+set :default_stage, "production"
+set :ssh_options, {:forward_agent => true}
+
+set :application, 'formandsystem_cms'
+set :repo_url, 'git@github.com:lukasoppermann/formandsystem-cms.git'
+set :user, "lukasoppermann"
+
+set :linked_dirs, %w(storage vendor)
+
+namespace :deploy do
+
+
+    desc 'Print The Server Name'
+    task :print_server_name do
+      on roles(:app), in: :groups, limit:1 do
+        execute "hostname"
+      end
+    end
+
+end
+
+after "deploy:updated", "deploy:print_server_name"
