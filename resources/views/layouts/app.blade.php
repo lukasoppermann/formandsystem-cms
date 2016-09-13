b72c2e5405ed8212fbdb9dbaea51d5822d45eef1
Lukas Oppermann <lukas@vea.re>
2016-09-13 15:39:55 +0200

    add https possiblility to asset

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
         <meta name="theme-color" content="rgb(255,210,0)">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="site_url" content="{{ url('/') }}">
-        <link href="{{ asset(env('FILE_PREFIX').elixir('css/app.css')) }}" rel="stylesheet" type="text/css">
+        <link href="{{ asset(env('FILE_PREFIX').elixir('css/app.css'), Request::secure()) }}" rel="stylesheet" type="text/css">
 
         @include('custom-css')
         <link href='//fonts.googleapis.com/css?family=Merriweather:300,700%7CLato:400,700&subset=latin,latin' rel='stylesheet' type='text/css'>
@@ -36,5 +36,5 @@
             @yield('content')
         </main>
     </body>
-    <script src='{{ asset(env('FILE_PREFIX').elixir("js/app.js")) }}'></script>
+    <script src='{{ asset(env('FILE_PREFIX').elixir("js/app.js"), Request::secure()) }}'></script>
 </html>
