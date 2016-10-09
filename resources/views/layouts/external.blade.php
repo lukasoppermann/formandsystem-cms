53e7567956cab44deac631a9bd9f6da1552fa40c
Lukas Oppermann <lukas@vea.re>
2016-10-09 15:41:58 +0200

    update GA traching code

--- resources/views/layouts/external.blade.php
+++ resources/views/layouts/external.blade.php
@@ -13,7 +13,7 @@
               (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
               m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
               })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-              ga('create', 'UA-7074034-24', 'auto');
+              ga('create', 'UA-7074034-25', 'auto');
               ga('require', 'linkid');
               ga('send', 'pageview');
               ga('set', 'anonymizeIp', true);
